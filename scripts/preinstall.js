f38aeeb4aaf41889c6a0ba4ad6d271cd8ea4b636
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-12-29 18:03:31 -0300

    feat: added general improvements.

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,8 +1,8 @@
-const { execSync } = require("child_process");
 const { mkdirSync, writeFileSync, chmodSync } = require("fs");
-const os = require("os");
+const { execSync } = require("child_process");
 const { resolve, dirname } = require("path");
 const request = require("sync-request");
+const os = require("os");
 
 const platform = os.platform();
 const architecture = os.arch();
@@ -26,19 +26,26 @@ if (patch !== null) {
 
         mkdirSync(dirname(outputPath), { recursive: true });
         writeFileSync(outputPath, response.getBody());
+        chmodSync(outputPath, 0o755); // Grant execute permissions to the binary
 
-        // Grant execute permissions to the binary
-        chmodSync(outputPath, 0o755);
-
-        console.log(`Galadriel CSS binary installed at: ${outputPath}`);
+        if (existsSync(outputPath)) {
+            console.log(`Galadriel CSS binary installed at: ${outputPath}`);
+        } else {
+            console.error("Galadriel CSS binary not found!");
+        }
     } catch (error) {
         console.error("Error during installation:", error.message);
         console.error("Stack trace:", error.stack);
+
+        throw error;
     }
 }
 
 function verifyOS(platform, architecture) {
-    console.log(`Detecting OS and architecture: ${platform} - ${architecture}`);
+    console.log(
+        PRINT_TAB,
+        `Detecting OS and architecture: ${platform} - ${architecture}`
+    );
 
     if (platform === "darwin") {
         return architecture.includes("x64")
@@ -47,22 +54,25 @@ function verifyOS(platform, architecture) {
     } else if (platform === "win32" && architecture.includes("x64")) {
         return "x86_64-pc-windows-msvc";
     } else if (platform === "linux" && architecture.includes("x64")) {
-        try {
-            const result = execSync("ldd $(which ls)", { encoding: "utf-8" });
-
-            if (result.includes("musl")) {
-                return "x86_64-unknown-linux-musl";
-            } else if (result.includes("libc.so")) {
-                return "x86_64-unknown-linux-gnu";
-            }
-        } catch (error) {
-            console.error("Error detecting Linux distribution:", error.message);
-        }
+        return this.detectLinuxDistro();
     }
 
-    console.log(
-        "Your operating system is currently unsupported. Galadriel CSS cannot run on this system."
-    );
+    console.error("Unsupported OS or architecture. Galadriel CSS cannot run.");
 
     return null;
+}
+
+function detectLinuxDistro() {
+    try {
+        const result = execSync("ldd $(which ls)", { encoding: "utf-8" });
+
+        if (result.includes("musl")) {
+            return "x86_64-unknown-linux-musl";
+        }
+    } catch (error) {
+        console.error("Error detecting Linux distro:", error.message);
+        console.log("Defaulting to x86_64-unknown-linux-gnu.");
+    }
+
+    return "x86_64-unknown-linux-gnu";
 }
